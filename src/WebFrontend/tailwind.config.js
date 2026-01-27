0f801b18636dde5f16e470851deae60d327da51a
Bryan Carlo Miguel <bryancarlomiguel@gmail.com>
2026-01-26 19:39:46 -0500

    initializes frontend

---
+++ src/WebFrontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.razor",
+    "./Layout/**/*.razor",
+    "./Pages/**/*.razor",
+    "./wwwroot/**/*.html",
+    "./wwwroot/**/*.js",
+    "./App.razor"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
